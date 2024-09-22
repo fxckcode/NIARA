b5ff703a6d0efeb83fc71a39604876471e470bb0
fxckcode <diegoale8582@hotmail.com>
2024-09-22 16:08:15 -0500

    add endpoint

--- src/services/login.services.ts
+++ src/services/login.services.ts
@@ -1,7 +1,7 @@
 import axiosClient from "../utils/axios"
 
 export const login = async (email: string, password: string) => {
-    await axiosClient.post('/auth/login', { email, password }).then((response) => {
+    await axiosClient.post('/login', { email, password }).then((response) => {
         if (response.status === 200) {
             window.location.href = '/dashboard'
         }
